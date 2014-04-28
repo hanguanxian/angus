ab78e70d3d42964e6115a3de9ba4e7d1e5d54227
hanguanxian <763877917@qq.com>
2014-04-28 13:13:39 +0800

    4.28

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,10 +3,10 @@ class StoreController < ApplicationController
   def index
   	if params[:search]
 	  	@productes = Product.paginate :page => params[:page],
-                                      :per_page => 6,
+                                      :per_page => 8,
                                       :conditions => ["pro_species like ?", "%#{params[:search]}%"] 
 	else             
-	    @productes =  Product.paginate(:page => params[:page], :per_page => 6)
+	    @productes =  Product.paginate(:page => params[:page], :per_page => 8)
 	end 
     @cart = current_cart
   end
