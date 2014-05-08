af42bb8bb412a12f4a69720b73720b78f4e89fd3
hanguanxian <763877917@qq.com>
2014-05-08 11:03:15 +0800

    5.8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,8 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    debugger
-    @user = User.new(user_params)
-
+    params.permit!
+    @user = User.new(params[:user])
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
